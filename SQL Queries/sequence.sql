56f20396a5ac920a3ca29f958643a78957c6122c
Savi0301 <savita.navalgi9736@gmail.com>
2023-03-19 00:01:37 -0400

    Create sequence.sql

---
+++ SQL Queries/sequence.sql
@@ -0,0 +1,9 @@
+drop sequence paymentid_seq;		
+drop sequence messageid_seq;		
+drop sequence oid_seq;	
+drop sequence rev_seq;	
+
+create sequence paymentid_seq start with 6;
+create sequence messageid_seq start with 6;
+create sequence oid_seq start with 7;
+create sequence rev_seq;
